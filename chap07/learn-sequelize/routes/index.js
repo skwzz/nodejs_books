119977f868d32661c68a486d6ecd7ad0d3e541aa
skwzz <kidol9108@gmail.com>
2020-08-09 16:23:42 +0900

    7장

--- chap07/learn-sequelize/routes/index.js
+++ chap07/learn-sequelize/routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
+var User = require("../models").User;
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    User.findAll()
+        .then((users) => {
+            res.render("sequelize", { users });
+        })
+        .catch((err) => {
+            console.error(err);
+            next(err);
+        });
 });
 
+/* await / async version */
+/*
+router.get("/", async (req, res, next)=>{
+  try{
+    const users = await User.findAll();
+    res.render('sequelize', {users});
+  }catch(error){
+    console.error(error);
+    next(error);
+  }
+});
+*/
 module.exports = router;
--- chap07/learn-sequelize/routes/users.js
+++ chap07/learn-sequelize/routes/users.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
+var User = require("../models").User;
+
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async (req, res, next) => {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+router.post("/", async (req, res, next) => {
+    try {
+        const result = User.create({
+            name: req.body.name,
+            age: req.body.age,
+            married: req.body.married,
+        });
+        console.log(result);
+        res.status(201).json(result);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 });
 
 module.exports = router;
